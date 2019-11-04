818e509051f7f006531cb240f9d36841f7665646
AudreyJones <audrey.eliz.jones@gmail.com>
2019-11-04 17:11:10 -0500

    First commit to Portfolio Page official repo

---
+++ Documents/Dev/portfolio-Page/ProjectData.js
@@ -0,0 +1,30 @@
+knitpickr = {
+    name: 'Knitpickr',
+    description: 'This web application uses a Ruby on Rails API backend, PostGreSQL database, and a JavaScript-based React/Redux user interface to track projects and materials needed for a user’s knitting/crochet/macrame projects.',
+    github: 'https://github.com/AudreyJones/Knitpickr.git',
+    demo: 'https://drive.google.com/file/d/1DPII7fs34gvMAXPCAtmpvkhdsUny02DY/view?usp=sharing'
+}
+
+adventure = {
+    name: 'AdventureApp',
+    description: 'This Rails web app allows users to go on an adventure, starring as the hero. Create, edit, update, and delete adventures featuring villains from popular franchises and organizations, including Harry Potter, Star Wars, and the Marvel Cinematic Universe. Search for a particular villain or franchise by name to access information more quickly.',
+    github: 'https://github.com/AudreyJones/AdventureApp.git',
+    demo: 'https://drive.google.com/file/d/13gx3bBUHfwG9fdbiFKC6D6xIVqTMNPia/view?usp=sharing'
+
+}
+
+coffeeshop = {
+    name: 'CoffeeShop App',
+    description: 'A Rails web app that enables users to create customized coffee orders containing multiple specialty coffee drinks. Users can update, favorite, and delete orders to customize their preferences.',
+    github: 'https://github.com/AudreyJones/CoffeeShop.git',
+    demo: 'https://drive.google.com/file/d/1tNpKa5NaT7ZWhRPoYpiRcfcMqQux8AfD/view?usp=sharing'
+
+}
+
+// doer = {
+//     name: 'Knitpickr',
+//     description: 'This web application uses a Ruby on Rails API backend, PostGreSQL database, and a JavaScript-based React/Redux user interface to track projects and materials needed for a user’s knitting/crochet/macrame projects.',
+//     github: 'https://github.com/AudreyJones/Knitpickr.git',
+//     demo: 'https://drive.google.com/file/d/1DPII7fs34gvMAXPCAtmpvkhdsUny02DY/view?usp=sharing'
+
+// }
